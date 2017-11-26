7d9d0460a749f27dd3561f9ac6708437b724abe9
alan <alan4chen@kimo.com>
2017-11-26 22:51:48 +0800

    test

--- NN_hw3/Q_Demo/Q_Demo.m
+++ NN_hw3/Q_Demo/Q_Demo.m
@@ -154,8 +154,12 @@ function varargout = pushbutton1_Callback(h, eventdata, handles, varargin)
 
 
 	while success<100000
-        [q_val,pre_state,pre_action,cur_state,cur_action] = get_action(x,v_x,theta,v_theta,reinf,q_val,pre_state,cur_state,pre_action,cur_action,ALPHA,BETA,GAMMA);
+        % [q_val,pre_state,pre_action,cur_state,cur_action] = get_action(x,v_x,theta,v_theta,reinf,q_val,pre_state,cur_state,pre_action,cur_action,ALPHA,BETA,GAMMA);
         
+        x_vec = Box(x,v_x,theta,v_theta, NUM_BOX);
+        [reward_hat, p_before] = ACE(BETAACE, 0.8, 0, 0.95, p_before);
+        cur_action = ASE(1000, 0.9, reward_hat);
+
         if (cur_action==1)   % push left
             F=-1*Force;
         else  F=Force;    % push right
